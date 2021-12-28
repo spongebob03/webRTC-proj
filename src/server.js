b9660d7db509973197a4176d5b04710737d4e72a
spongebob03 <bmofinnjake08@gmail.com>
2021-12-28 17:13:01 +0900

    send message with socket

--- src/server.js
+++ src/server.js
@@ -15,13 +15,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Brower");
     socket.on("close", () => console.log("Disconnected from the Brower"));
     socket.on("message", (message) => {
-        console.log(message);
+        // console.log(message.toString('utf8'));
+        sockets.forEach((aSocket) => aSocket.send(message.toString('utf8')));
     });
-    socket.send("hello!!");
 });
 
 server.listen(3000, handleListen);
